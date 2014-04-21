408b06ddd7c0fab67b3fb563e63baa8f1ed68929
Avenger7x <avenger7x13@gmail.com>
2014-04-21 15:14:12 +0300

    wrap for browser

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,9 @@
 var gulp = require('gulp'),
 	jshint = require('gulp-jshint'),
 	complexity = require('gulp-complexity'),
-	karma = require('gulp-karma');
+	karma = require('gulp-karma'),
+	wrap = require('gulp-wrap-exports'),
+	rename = require('gulp-rename');
 
 gulp.task('analysis', function(){
 	return gulp.src('*.js')
@@ -18,6 +20,13 @@ gulp.task('test', function(){
 			configFile: 'karma.conf.js',
 			action: 'run'
 		}));
+});
+
+gulp.task('wrap', function(){
+	return gulp.src('index.js')
+		.pipe(wrap({ name: 'walk' }))
+		.pipe(rename('walker.js'))
+		.pipe(gulp.dest('.'));
 });
 
 gulp.task('default', ['analysis', 'test']);
---
+++ walker.js
@@ -0,0 +1,40 @@
+!function(module, global){
+var exports = module.exports;
+/* jshint browser:true */
+'use strict';
+
+function createTreeWalker(root, acceptNode, whatToShow){
+	acceptNode = acceptNode || function(){ return NodeFilter.FILTER_ACCEPT; };
+	whatToShow = whatToShow || NodeFilter.SHOW_ELEMENT;
+
+	return document.createTreeWalker(root, whatToShow, { acceptNode: acceptNode }, false);
+}
+
+function createIterator(walker, next){
+	return function iterator(direction){
+		switch(direction){
+			case 'sibling':
+				return next(walker.nextSibling(), iterator);
+			case 'node':
+				/* falls through */
+			default:
+				return next(walker.nextNode(), iterator);
+		}
+	};
+}
+
+module.exports = function(root, process, options){
+	var walker;
+
+	options = options || {};
+	walker = createTreeWalker(root, options.acceptNode, options.whatToShow);
+
+	function next(node, iterator){
+		return node && process(node, iterator);
+	}
+
+	next(walker.currentNode, createIterator(walker, next));
+};
+
+global.walk = module.exports;
+}({ exports: {} }, function(){ return this; }());
