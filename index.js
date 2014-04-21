d7b8ac7d1ddd3625f56320b188733679de9beda2
Avenger7x <avenger7x13@gmail.com>
2014-04-21 10:42:27 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+function createTreeWalker(root, acceptNode, whatToShow){
+	acceptNode = acceptNode || function(){ return NodeFilter.FILTER_ACCEPT; };
+	whatToShow = whatToShow || NodeFilter.SHOW_ELEMENT;
+
+	return document.createTreeWalker(root, whatToShow, { acceptNode: acceptNode }, false);
+}
+
+module.exports = function(root, process, options){
+	var walker = createTreeWalker(root, options.acceptNode, options.whatToShow),
+		node = walker.currentNode;
+};
---
+++ readme.md
@@ -0,0 +1,29 @@
+# dom-walker
+
+DOM Walker
+
+## Install
+
+```bash
+npm install dom-walker
+```
+
+```bash
+component install alexeyraspopov/dom-walker
+```
+
+```bash
+bower install dom-walker
+```
+
+## API
+
+TBD
+
+## Usage
+
+TBD
+
+## License
+
+[MIT License](http://en.wikipedia.org/wiki/MIT_License) (c) Alexey Raspopov
